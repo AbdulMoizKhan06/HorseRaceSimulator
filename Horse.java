2967d20ec8bc8a77ae4eca2b351d499595cb7c38
Abdul Khan <ec24499@qmul.ac.uk>
2025-04-27 18:49:27 +0100

    Update Horse.java 1.0, added methods and fields
    Set up all fields and necessary methods.

--- Horse.java
+++ Horse.java
@@ -1,13 +1,19 @@
 
 /**
- * Write a description of class Horse here.
+ * Class to represent individual race participants as horses
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Abdul Moiz Khan
+ * @version 1.0
  */
 public class Horse
 {
     //Fields of class Horse
+
+    String name;
+    char symbol;
+    double confidence;
+    boolean fallen;
+    int DistanceTravelled;
     
     
       
@@ -17,60 +23,62 @@ public class Horse
      */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
-       
+       this.symbol = horseSymbol;
+       this.name = horseName;
+       this.confidence = horseConfidence;
     }
     
     
     
     //Other methods of class Horse
-    public void fall()
+    public void fall() // Sets horse as fallen
     {
-        
+        this.fallen = true;
     }
     
-    public double getConfidence()
+    public double getConfidence() // Returns horse confidence rating
     {
-        
+        return this.confidence;
     }
     
-    public int getDistanceTravelled()
+    public int getDistanceTravelled() // Returns distance travelled by horse
     {
-        
+        return this.DistanceTravelled;
     }
     
-    public String getName()
+    public String getName() // Returns horse name
     {
-        
+        return this.name;
     }
     
-    public char getSymbol()
+    public char getSymbol() // Returns horse symbol
     {
-        
+        return this.symbol;
     }
     
-    public void goBackToStart()
+    public void goBackToStart() // Resets the horse to the start of the race
     {
-        
+        this.DistanceTravelled = 0; //?
     }
     
-    public boolean hasFallen()
+    public boolean hasFallen() // Returns true if horse has fallen, false otherwise
     {
-        
+        return this.fallen;
     }
 
-    public void moveForward()
+    public void moveForward() // Increments the distance travelled by the horse by 1
     {
-        
+        this.DistanceTravelled += 1;
     }
 
-    public void setConfidence(double newConfidence)
+    public void setConfidence(double newConfidence) // Sets confidence rating of the horse to given value
     {
-        
+        this.confidence = newConfidence;
     }
     
-    public void setSymbol(char newSymbol)
+    public void setSymbol(char newSymbol) // Sets horse symbol to given character
     {
-        
+        this.symbol = newSymbol;
     }
     
 }
